8625a40e9d10f80d9b29f729782a10b093e27e7f
Nick <N_97@mail.ru>
2023-02-11 16:21:37 +0300

    делаем уроки

--- main_script.py
+++ main_script.py
@@ -5,34 +5,40 @@ import pydicom
 
 
 def person_names_callback(dataset):
+    """person anonymize"""
     dataset['PatientName'].value = "Anonymous"
 
 
-def main():
+def main() -> None:
+    """file refactoring"""
     with open('tree.csv', 'w', encoding='UTF-8') as csvfile:
         directory = 'src'
         fieldnames = ['Source path', 'Output path']
         writer = csv.DictWriter(csvfile, dialect='excel', fieldnames=fieldnames)
         writer.writeheader()
         for filename in os.listdir(directory):
-            f = os.path.join(directory, filename)
-            ds = pydicom.dcmread(f, force=True)
-            writer.writerow({'Source path': f,
-                             'Output path': f'output\{ds["StudyInstanceUID"].value}\{ds["SeriesInstanceUID"].value}/{ds["SOPInstanceUID"].value}.dcm'})
+            file_path = os.path.join(directory, filename)
+            dicom_data = pydicom.dcmread(file_path, force=True)
+            writer.writerow({'Source path': file_path,
+                             'Output path': f'output/{dicom_data["StudyInstanceUID"].value}/'
+                                            f'{dicom_data["SeriesInstanceUID"].value}/'
+                                            f'{dicom_data["SOPInstanceUID"].value}.dcm'})
             try:
-                os.makedirs(f'output\{ds["StudyInstanceUID"].value}\{ds["SeriesInstanceUID"].value}')
-                person_names_callback(ds)
+                os.makedirs(f'output/{dicom_data["StudyInstanceUID"].value}/'
+                            f'{dicom_data["SeriesInstanceUID"].value}')
+                person_names_callback(dicom_data)
             except OSError:
                 print('Directory already exists.')
             except KeyError:
                 print('Patient name does not exist???')
             finally:
-                ds.save_as(
-                    f'output\{ds["StudyInstanceUID"].value}\{ds["SeriesInstanceUID"].value}\{ds["SOPInstanceUID"].value}.dcm')
+                dicom_data.save_as(
+                    f'output/{dicom_data["StudyInstanceUID"].value}/'
+                    f'{dicom_data["SeriesInstanceUID"].value}/'
+                    f'{dicom_data["SOPInstanceUID"].value}.dcm')
         csvfile.close()
-        read_file = pd.read_csv (r'tree.csv')
-        read_file.to_excel (r'tree.xlsx', index = None, header=True)
-    return 0
+        read_file = pd.read_csv(r'tree.csv')
+        read_file.to_excel(r'tree.xlsx', index=None, header=True)
 
-
-main()
+if __name__ == '__main__':
+    main()
